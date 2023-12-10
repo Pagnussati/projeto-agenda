4a3859b672bf646a9780418870f9e5a9c4509b1b
Juaum <joaogpagnussati@gmail.com>
2023-12-10 09:45:40 -0300

    Criptografando senha no banco de dados

--- source/models/loginModel.js
+++ source/models/loginModel.js
@@ -1,9 +1,10 @@
-const validator = require('validator')
+const validator = require('validator');
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
-    password: { type: String, required: true},
+    password: { type: String, required: true },
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
@@ -17,25 +18,35 @@ class Login {
 
     async register() {
         this.validate();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
 
-        try{
+        await this.newUser();
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+        try {
             this.user = await LoginModel.create(this.body)
-        } catch(e){
+        } catch (e) {
             console.log(e);
         };
+    }
+
+    async newUser() {
+        const user = LoginModel.findOne({ email: this.body.email });
+        if (user) this.errors.push('Usuário já existe');
     }
 
     validate() {
         this.cleanUp();
 
-        if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+        if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
 
-        if(this.body.password.length < 3 || this.body.password.length >= 50) this.errors.push('Senha necessita estar entre 3 e 50 caracteres')
+        if (this.body.password.length < 3 || this.body.password.length >= 50) this.errors.push('Senha necessita estar entre 3 e 50 caracteres')
     }
 
     cleanUp() {
-        for(const key in this.body) {
+        for (const key in this.body) {
             if (typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
