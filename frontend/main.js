593037d239146acc78529569aba911853bb45da1
Juaum <joaogpagnussati@gmail.com>
2024-01-17 15:14:12 -0300

    validando frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,5 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
 
-import './assets/css/style.css';
+//import './assets/css/style.css';
+
---
+++ frontend/modules/loginValidate.js
@@ -0,0 +1,7 @@
+import validator from "validator";
+
+export default class LoginValidate {
+    constructor(formClass) {
+        this.form = document.querySelector(formClass);
+    }
+}
