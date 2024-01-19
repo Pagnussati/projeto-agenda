ac55e4f9819b2f200008bb9ace7646101090b4b2
Juaum <joaogpagnussati@gmail.com>
2024-01-19 17:36:41 -0300

    correcao

--- frontend/main.js
+++ frontend/main.js
@@ -2,4 +2,3 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime'
 
 //import './assets/css/style.css';
-
--- frontend/modules/loginValidate.js
+++
@@ -1,7 +0,0 @@
-import validator from "validator";
-
-export default class LoginValidate {
-    constructor(formClass) {
-        this.form = document.querySelector(formClass);
-    }
-}
